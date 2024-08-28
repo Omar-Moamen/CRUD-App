243c0444148b258b21bcd67e716d9a866597a35f
OmarMoamen <omarmoamen911@gmail.com>
2024-08-28 15:36:40 +0300

    fixed error page

--- crud-main/src/pages/Error.tsx
+++ crud-main/src/pages/Error.tsx
@@ -1,38 +1,32 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import LottieHandler from '../components/feedback/LottieHandler/LottieHandler';
 
 const Error = () =>
 {
    return (
-      <Container maxWidth="md">
-         <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="space-around"
-            position="absolute"
-            top="40%"
-            left="50%"
-            sx={{ translate: "-50% -50%" }}
+      <Container
+         maxWidth="md"
+         sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: "center",
+            textAlign: "center",
+            pt: "8rem",
+         }}
+      >
+         <LottieHandler type={"notFound"} width='450px' />
+
+         <Typography
+            component={NavLink}
+            to="/"
+            variant="h6"
+            color="primary"
+            display="block"
+            sx={{ textDecoration: "none" }}
          >
-
-            <LottieHandler type={"notFound"} width='450px' />
-
-            <Box>
-               <Typography
-                  component={NavLink}
-                  to="/"
-                  variant="h6"
-                  color="primary"
-                  display="block"
-                  mt="15rem"
-                  sx={{ textDecoration: "none" }}
-               >
-                  How about returning to safety?
-               </Typography>
-            </Box>
-         </Box>
+            How about returning to safety?
+         </Typography>
       </Container>
    )
 }
